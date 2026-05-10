6953a4f897875e55fda4715136569888768b54b6
Sai Shanmukha <nss10@outlook.com>
2026-05-10 15:21:55 -0500

    cause exec_error to maje a sys_error

--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"errors"
 	"os"
 
 	"github.com/ohsu-comp-bio/funnel/cmd"
 	"github.com/ohsu-comp-bio/funnel/logger"
-	"github.com/ohsu-comp-bio/funnel/worker"
 )
 
 func main() {
@@ -17,10 +15,10 @@ func main() {
 		// worker catches the error and updates the task state, but the worker itself should not
 		// fail. This avoids unwanted worker retries when the issue is a failure in the user's
 		// script run by the executor.
-		var execErr *worker.K8sExecutorErr
-		if errors.As(err, &execErr) {
-			os.Exit(0)
-		}
+		// var execErr *worker.K8sExecutorErr
+		// if errors.As(err, &execErr) {
+		// 	os.Exit(0)
+		// }
 
 		os.Exit(1)
 	}
